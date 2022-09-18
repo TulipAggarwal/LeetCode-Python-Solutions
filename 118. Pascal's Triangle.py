e48fa020398b02cba91053c4958e3984bed2d15d
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-09-18 21:49:55 +0530

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -5,7 +5,7 @@ class Solution:
             temp = [0] + res[-1] + [0]
             row = []
             
-            for j in range(len(res[-1])+1):
+            for j in range(len(res[-1])+1) :
                 row.append(temp[j]+temp[j+1])
             res.append(row)
         return res
