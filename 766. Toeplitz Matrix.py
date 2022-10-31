49b2dfc6144d2d9f508c2fe439687af97d7a3694
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-10-31 16:54:13 +0530

    Create 766. Toeplitz Matrix.py

---
+++ 766. Toeplitz Matrix.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i-1][j-1] != matrix[i][j]:
+                    return False
+        return True
